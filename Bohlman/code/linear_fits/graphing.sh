1d25d86ed0c48f305ec92e3e297053c05868c573
Chris Bohlman <cbohlman@email.arizona.edu>
2017-11-07 13:38:36 -0700

    Added variance vs. mean value with linear fit plots to report all all data

--- Bohlman/code/linear_fits/graphing.sh
+++
@@ -1,1 +0,0 @@
-graph -T ps -L "median of variance vs. mean value with linear fits" -X "mean value" -Y "variance" -C data.out data1.out data2.out > plot.ps
---
+++ Bohlman/code/plot_all/graphing.sh
@@ -0,0 +1,1 @@
+graph -T ps -L "median of variance vs. mean value with linear fits" -X "mean value" -Y "variance" -C data.out data1.out data2.out data3.out data4.out data5.out data6.out data7.out > plot.ps
