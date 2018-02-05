4251e4f2eb3dc102123a3cbb7bc95121e1af0e18
Chris Bohlman <cbohlman@email.arizona.edu>
2018-02-05 11:50:13 -0700

    Edited directories

--- Bohlman/code/usb_grab/write_basler_fits.h
+++ Bohlman/code/usb_grab/write_basler_fits.h
@@ -27,8 +27,7 @@ using namespace Basler_UsbCameraParams;
 
 using namespace Pylon;
 using namespace std;
-static const uint32_t c_countOfImagesToGrab = 100;
-static const uint32_t a_countOfImagesToGrab = 100;
+static const uint32_t c_countOfImagesToGrab = 10;
 
 /*! \struct image
 //struct that is passed to write_basler_fits() that has data for the image
