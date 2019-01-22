913e8151562cc5d6cc067a911d74d5f01eb15ff6
Jared Males <jaredmales@gmail.com>
2019-01-22 13:31:59 -0700

    updated to grab latest image only, for 0 latency in viewer

---
+++ labBasler/README.md
@@ -0,0 +1,5 @@
+
+1) Download the Basler Pylon SDK, which is available for free on baslerweb.com
+  -- Unpack it.
+
+2) Follow the instructions in the INSTALL to install to /opt.
--- labBasler/viewBasler/view1Basler.cpp
+++ labBasler/viewBasler/view1Basler.cpp
@@ -64,17 +64,20 @@ int main( int argc, ///< [in] the integer value of the count of the command line
       mx::improc::ds9Interface ds9(file_name);
       ds9.connect();
       
+      camera.RegisterConfiguration( new CAcquireContinuousConfiguration , RegistrationMode_ReplaceAll, Cleanup_Delete);
+      
       camera.Open(); // Opens camera parameters to grab images and set exposure time
       camera.ExposureAuto.SetValue(ExposureAuto_Off); // Set exposure
       camera.ExposureTime.SetValue(exposure);
       camera.PixelFormat.SetValue(PixelFormat_Mono10);
       
-      camera.StartGrabbing(); // Start grabbing a provided amount of images
+      camera.StartGrabbing(GrabStrategy_LatestImageOnly ); // Start grabbing a provided amount of images
    
+      CGrabResultPtr ptrGrabResult;
+      
       while (camera.IsGrabbing()) 
       {
-         CGrabResultPtr ptrGrabResult;
-      
+         
          int tempcam = (int)camera.DeviceTemperature.GetValue(); // Gets and stores temperature of camera
          camera.RetrieveResult(5000, ptrGrabResult, TimeoutHandling_ThrowException);  // Waits for an image and then retrieves it. A timeout of 5000 ms is used
          
