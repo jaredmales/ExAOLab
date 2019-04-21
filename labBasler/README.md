08154d66ff85f6d99d68b352ab13f119dc3e587a
Jared Males <jaredmales@gmail.com>
2019-04-20 17:03:45 -0700

    updated command line opts, ds9 now detaches

--- labBasler/README.md
+++ labBasler/README.md
@@ -1,5 +1,60 @@
 
-1) Download the Basler Pylon SDK, which is available for free on baslerweb.com
-  -- Unpack it.
+# Dependencies
 
-2) Follow the instructions in the INSTALL to install to /opt.
+## Basler Pylon SDK
+   1) Download the Basler Pylon SDK, which is available for free on baslerweb.com
+
+   2) Unpack it.
+
+   3) Follow the instructions in the INSTALL to install to `/opt`.
+
+## libxpa:
+```
+git clone https://github.com/ericmandel/xpa.git
+cd xpa
+./configure
+make
+sudo make install
+```
+
+## cfitsio:
+You should be able to install this from a package manager.  Be sure to get the `devel` version.  If not, build from source like so:
+```
+wget http://heasarc.gsfc.nasa.gov/FTP/software/fitsio/c/cfitsio3450.tar.gz
+tar -xvzf cfitsio3450.tar.gz
+cd cfitsio
+./configure --prefix=/usr/local
+make
+sudo make install
+```
+Note: change the version of the package to the latest one if you want.
+
+## saoimage ds9.  
+Currently (April 2019) ds9 version 7.5 works, but versions  >= 8 don't seem to work with libxpa anymore.
+
+# viewBasler
+## Build
+ Once the above installs are done,
+```
+$ cd viewBasler
+$ make
+```
+
+## Running view1Basler
+
+The `view1Basler` program takes 0 to 3 arguments.  If no arguments are specified, it opens the first Basler camera it finds and sets an exposure time of 1000 micro-seconds.  `ds9` will open and begin displaying the images.
+
+If just 1 argument is given, it is treated as an expsoure time in micro-seconds.
+```
+$ ./view1Basler
+```
+
+If 2 arguments are given, the first will be treated as the serial number of the camera to connect to, the second as exposure time in micro-seconds.
+```
+$ ./view1Basler 22411493 10000
+```
+
+If 3 arguments are give, the first is the serial number, the second is exposure time in micro-seconds, and the third is the name of the camera.
+```
+$ ./view1Basler  22411493 10000 cam1
+```
--- labBasler/viewBasler/view1Basler.cpp
+++ labBasler/viewBasler/view1Basler.cpp
@@ -3,6 +3,15 @@
 Initializes the pylon resources, takes the photos, and passes a struct to write_basler_fits() function with image data in it
 */
 
+#include <signal.h>
+#include <iostream>
+#include <algorithm>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <functional>
+
+
 #include <pylon/PylonIncludes.h>
 #ifdef PYLON_WIN_BUILD
 #    include <pylon/PylonGUI.h>
@@ -12,12 +21,6 @@ Initializes the pylon resources, takes the photos, and passes a struct to write_
 #include <pylon/usb/BaslerUsbInstantCamera.h>
 #include <pylon/usb/_BaslerUsbCameraParams.h>
 #include <GenApi/IFloat.h>
-#include <iostream>
-#include <algorithm>
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <functional>
 
 #define DS9INTERFACE_NO_EIGEN
 #include "../lib/mx/improc/ds9Interface.hpp"
@@ -28,6 +31,26 @@ using namespace Basler_UsbCameraParams;
 using namespace Pylon;
 using namespace std;
 
+
+
+bool timeToDie = false;
+
+void sigTermHandler( int signum,
+                     siginfo_t *siginf,
+                     void *ucont
+                   )
+{
+   std::cerr << "Exiting . . . \n";
+   //Suppress those warnings . . .
+   static_cast<void>(signum);
+   static_cast<void>(siginf);
+   static_cast<void>(ucont);
+  
+   timeToDie = true;
+}
+
+
+
 //  Main function
 /** Initializes pylon resources, takes pictures, closes all pylon resources.
 * \return an integer: 0 upon exit success, 1 otherwise
@@ -36,13 +59,33 @@ int main( int argc, ///< [in] the integer value of the count of the command line
           char* argv[] ///< [ch.ar] the integer value of the count of the command line arguments
         )
 {
+   std::string argv0 = argv[0];
    
+   /* First install signal handling */
+   struct sigaction act;
+   sigset_t set;
+
+   act.sa_sigaction = sigTermHandler;
+   act.sa_flags = SA_SIGINFO;
+   sigemptyset(&set);
+   act.sa_mask = set;
+
+   errno = 0;
+   if( sigaction(SIGTERM, &act, 0) < 0 )
+   {
+      std::cerr << " (" << argv0 << "): error setting SIGTERM handler: " << strerror(errno) << "\n";
+      return -1;
+   }
+   
+   
+   /* Now process args */
    typedef int16_t pixelT;
    
    int exitCode = 0;
-   int exposure = 0;
+   int exposure = 1000;
    
-   std::string sernum;
+   std::string serNum; // Serial number of camera to connect to
+   std::string camName;
    
    if (argc == 2) 
    {                                // if there are more than 2 command line arguments
@@ -50,13 +93,16 @@ int main( int argc, ///< [in] the integer value of the count of the command line
    }   
    else if (argc == 3) 
    {                                
-      sernum = argv[1]; 
+      serNum = argv[1]; 
       exposure = atoi(argv[2]);
    }
-   else 
-   {  // if there are less than 2 command line arguments set default
-      exposure = 1000;  // default value of exp: 5000 us
+   else if (argc == 4)
+   {
+      serNum = argv[1]; 
+      exposure = atoi(argv[2]);
+      camName = argv[3];
    }
+
    
    PylonInitialize(); // Initializes pylon runtime before using any pylon methods
    
@@ -65,28 +111,37 @@ int main( int argc, ///< [in] the integer value of the count of the command line
       CDeviceInfo info; // Set up attached Basler USB camera
       info.SetDeviceClass(Camera_t::DeviceClass());
       
-      if(sernum != "") info.SetSerialNumber(sernum.c_str());
+      if(serNum != "") info.SetSerialNumber(serNum.c_str());
       CBaslerUsbInstantCamera camera( CTlFactory::GetInstance().CreateFirstDevice(info));
-      string file_name = (string)camera.GetDeviceInfo().GetModelName() + "_" + (string)camera.GetDeviceInfo().GetSerialNumber(); // Gets camera model name and serial number
-      std::cerr << file_name << std::endl;
       
-      mx::improc::ds9Interface ds9(file_name);
+      std::cerr << "Found camera: " << (string)camera.GetDeviceInfo().GetModelName() << "\n";
+      std::cerr << "Serial number: " << (string)camera.GetDeviceInfo().GetSerialNumber() << "\n";
+      
+      if(camName == "")
+      {
+         camName = (string)camera.GetDeviceInfo().GetModelName() + "_" + (string)camera.GetDeviceInfo().GetSerialNumber();
+      }
+      
+      std::cerr << "Camera name: " << camName << "\n";
+      
+      mx::improc::ds9Interface ds9(camName);
       ds9.connect();
       
       camera.RegisterConfiguration( new CAcquireContinuousConfiguration , RegistrationMode_ReplaceAll, Cleanup_Delete);
       
       camera.Open(); // Opens camera parameters to grab images and set exposure time
+      camera.PixelFormat.SetValue(PixelFormat_Mono10); //Make it 10 bit
+      
       camera.ExposureAuto.SetValue(ExposureAuto_Off); // Set exposure
       camera.ExposureTime.SetValue(exposure);
-      camera.PixelFormat.SetValue(PixelFormat_Mono10);
+
+
       
       camera.StartGrabbing(GrabStrategy_LatestImageOnly ); // Start grabbing a provided amount of images
    
       CGrabResultPtr ptrGrabResult;
       
-     
-   
-      while (camera.IsGrabbing()) 
+      while (camera.IsGrabbing() && !timeToDie) 
       {
          CGrabResultPtr ptrGrabResult;
       
@@ -103,7 +158,8 @@ int main( int argc, ///< [in] the integer value of the count of the command line
          else  // If image is not grabbed successfully, throw an error
          {
             cerr << "Error: " << ptrGrabResult->GetErrorCode() << " " << ptrGrabResult->GetErrorDescription() << endl;
-            exitCode = 1;
+            exitCode = -1;
+            break;
          }
       }
       camera.Close();
@@ -112,8 +168,10 @@ int main( int argc, ///< [in] the integer value of the count of the command line
    {
       cerr << "An exception occurred." << endl
          << e.GetDescription() << endl;
-      exitCode = 1;
+      exitCode = -1;
    }
+   
    PylonTerminate();   // Releases all pylon resources. 
+   
    return exitCode;
 }
